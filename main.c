165f74be22370f904639dbe6f44ea0b189134cbf
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-02-07 02:31:39 +0600

    Handle multiple devices

--- main.c
+++ main.c
@@ -221,42 +221,42 @@ main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    if (device_files[1])
-    {
-        g_printerr("Only one device file is accepted");
-        return EXIT_FAILURE;
-    }
-
-    g_autoptr(LiquidHidDevice) device
-        = liquid_hid_device_new_for_path(device_files[0], HID_MAX_BUFFER_SIZE, &error);
-    if (!device)
-    {
-        g_printerr("Can't open device: %s\n", error->message);
-        return EXIT_FAILURE;
-    }
-
     g_autofree gchar *fan_config_signal = g_strdup_printf("input-report::%#x", INPUT_REPORT_ID_FAN_CONFIG);
-    g_signal_connect(device, fan_config_signal, G_CALLBACK(input_report_fan_config), NULL);
-
     g_autofree gchar *fan_status_signal = g_strdup_printf("input-report::%#x", INPUT_REPORT_ID_FAN_STATUS);
-    g_signal_connect(device, fan_status_signal, G_CALLBACK(input_report_fan_status), NULL);
 
-    g_signal_connect(device, "input-report", G_CALLBACK(input_report_unknown), NULL);
+    g_autolist(LiquidHidDevice) devices = NULL;
 
-    if (init_device)
+    for (GStrv device_file = device_files; *device_file; device_file++)
     {
-        if (liquid_hid_device_output_report(device, detect_fans_report, OUTPUT_REPORT_SIZE, &error)
-            != OUTPUT_REPORT_SIZE)
+        LiquidHidDevice *device = liquid_hid_device_new_for_path(*device_file, HID_MAX_BUFFER_SIZE, &error);
+
+        if (device == NULL)
         {
-            g_printerr("Failed to send detect fans command: %s\n", error->message);
+            g_printerr("Can't open device %s: %s\n", *device_file, error->message);
             return EXIT_FAILURE;
         }
 
-        if (liquid_hid_device_output_report(device, set_update_interval_report, OUTPUT_REPORT_SIZE, &error)
-            != OUTPUT_REPORT_SIZE)
+        devices = g_list_prepend(devices, device);
+
+        g_signal_connect(device, fan_config_signal, G_CALLBACK(input_report_fan_config), NULL);
+        g_signal_connect(device, fan_status_signal, G_CALLBACK(input_report_fan_status), NULL);
+        g_signal_connect(device, "input-report", G_CALLBACK(input_report_unknown), NULL);
+
+        if (init_device)
         {
-            g_printerr("Failed to send update interval command: %s\n", error->message);
-            return EXIT_FAILURE;
+            if (liquid_hid_device_output_report(device, detect_fans_report, OUTPUT_REPORT_SIZE, &error)
+                != OUTPUT_REPORT_SIZE)
+            {
+                g_printerr("Failed to send detect fans command to %s: %s\n", *device_file, error->message);
+                return EXIT_FAILURE;
+            }
+
+            if (liquid_hid_device_output_report(device, set_update_interval_report, OUTPUT_REPORT_SIZE, &error)
+                != OUTPUT_REPORT_SIZE)
+            {
+                g_printerr("Failed to send update interval command to %s: %s\n", *device_file, error->message);
+                return EXIT_FAILURE;
+            }
         }
     }
 
